bb1b90e5744b31f0c8c8bc1ae0f3cd8ff6bc44dd
Nikhil <nikhil.4002.50.82@gmail.com>
2025-07-24 23:57:28 +0530

    modified header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -3,7 +3,7 @@ import { GiChatBubble } from "react-icons/gi";
 
 const Header = () => {
   return (
-    <div className="w-full flex items-center px-3 sm:px-4 md:px-6 py-3 sm:py-4 md:py-5 bg-blue-500">
+    <div className="w-full flex items-center px-3 sm:px-4 md:px-6 py-3 sm:py-4 md:py-5 bg-blue-600">
       <div className="flex items-center">
         <span>
           <GiChatBubble className="text-white font-semibold text-5xl" />
